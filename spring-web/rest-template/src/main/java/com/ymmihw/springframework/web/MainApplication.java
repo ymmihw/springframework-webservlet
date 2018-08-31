ef690ebefdc3fae0d2cea3772f0a2195a12e6e8d
wanghaoming <whimmy@126.com>
2018-08-31 11:53:03 +0800

    add rest-template

---
+++ spring-web/rest-template/src/main/java/com/ymmihw/springframework/web/MainApplication.java
@@ -0,0 +1,11 @@
+package com.ymmihw.springframework.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MainApplication {
+  public static void main(final String[] args) {
+    SpringApplication.run(MainApplication.class, args);
+  }
+}
