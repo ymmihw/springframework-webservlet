303d89654539c7d9c8adff03f370c5375662d4b6
wanghaoming <wanghaoming@ctfo.com>
2021-05-19 15:55:18 +0800

    add handler-interceptor-vs-filter

---
+++ spring-web/handler-interceptor-vs-filter/src/main/java/com/ymmihw/springframework/FilterInterceptorApp.java
@@ -0,0 +1,11 @@
+package com.ymmihw.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterInterceptorApp {
+  public static void main(String[] args) {
+    SpringApplication.run(FilterInterceptorApp.class, args);
+  }
+}
